411d1b5152bed4f4973cfb1523589bef05cf30c6
Sunzheini <daniel_zorov@abv.bg>
2023-12-05 08:50:16 +0200

    Update AddPyscriptForm.js
    Added validation to accept .py files only

--- src/components/Forms/AddPyscriptForm.js
+++ src/components/Forms/AddPyscriptForm.js
@@ -7,6 +7,33 @@ export default function AddPyscriptForm(props) {
         script_file: null,
     });
 
+    const handleFormSubmit = () => {
+        const scriptName = document.getElementById('script_name').value;
+        const scriptFileInput = document.getElementById('script_file');
+        const scriptFile = scriptFileInput.files[0];
+
+        // Check if a script_name is provided
+        if (!scriptName.trim()) {
+            alert('Please enter a Py Script name.');
+            return;
+        }
+
+        // Check if script_file is provided
+        if (!scriptFile) {
+            alert('Please choose a Py script file.');
+            return;
+        }
+
+        // Check if the file has a .py extension
+        if (!scriptFile.name.endsWith('.py')) {
+            alert('Please choose a file with a .py extension.');
+            return;
+        }
+
+        // If all validation is ok, call the onCreateHandler
+        props.onCreateHandler({script_name: scriptName, script_file: scriptFile});
+    };
+
     return (
         <div className="add-vm-form">
             <form className={"form-wrapper"} encType="multipart/form-data" method="POST">
@@ -41,13 +68,7 @@ export default function AddPyscriptForm(props) {
                         type="button"
                         value="Add Py Script"
                         className={"card-btn"}
-                        onClick={() => {
-                            const formData = {
-                                script_name: document.getElementById('script_name').value,
-                                script_file: document.getElementById('script_file').files[0],
-                            };
-                            props.onCreateHandler(formData);
-                        }}
+                        onClick={handleFormSubmit}
                     />
                 </div>
             </form>
